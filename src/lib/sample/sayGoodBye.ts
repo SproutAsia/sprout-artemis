a26bd37bbcb8f2b88033da246f98ba64ca19a74f
ricoarisandyw <arisandyrico@gmail.com>
2024-05-29 14:58:48 +0700

    feat: test docs on func and params

---
+++ src/lib/sample/sayGoodBye.ts
@@ -0,0 +1,11 @@
+/**
+ * function docs
+ */
+export function sayGoodbyeArtemis(args: {
+    /**
+     * first name documentation
+     */
+    firsName: string
+}) {
+    console.log("Thanks Artemis good bye", args.firsName)
+}
