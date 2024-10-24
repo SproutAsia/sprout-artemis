a5ec2098af7d9d9efba9e0b8dc89e7782ee32740
ruztyA <anggitaruzty21@gmail.com>
2024-10-24 13:09:43 +0700

    remove console.log at getUsers

--- src/artemis/v3/api/user/_get/getUsers.ts
+++ src/artemis/v3/api/user/_get/getUsers.ts
@@ -16,9 +16,6 @@ export default async function getUsers(args: {
     const headers = ArtemisHeader()
     headers.append("Authorization", "Bearer " + args.auth.token)
     const url = new URL(process.env.ARTEMIS_API + "/authentication-manager/user?page=0&size=1000&isActive=true&sort=updatedAt%2Cdesc&direction=desc")
-
-    console.log(">>> URL", url.toString())
-
     try {
         const result = await fetch(url.toString(), {
             headers
